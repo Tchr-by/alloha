b570875e4baf50552184fc9ed2febc87d4bf0860
Tchr <dmitriy_borbat@mail.ru>
2018-10-26 11:14:17 +0300

    add file template all_here adn modified file home.php

--- child-theme/clean-template-3-child/home.php
+++ child-theme/clean-template-3-child/home.php
@@ -4,16 +4,20 @@ get_header(); ?>
 
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
-            <h2>ЭТО ШАБЛОН home.php</h2>
+            <h2>ЭТО ШАБЛОН home.php который теперь кроме записей выводит и ПРОЕКТЫ!</h2>
 
-            <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <h2><?php the_title(); ?></h2>
-                    <h3><?php the_content(); ?></h3>
-                    <h6><?php the_author_posts_link()?> </h6>
-                </article>
+            <?php
+            $my_posts = get_posts( array(
+                'numberposts' => -1,
+                'post_type' => array( 'post' , 'projects' ),
+                'orderby' => 'title',
+            ) ); // вывод всех записай и проектов с сортировкой по названию
+            foreach ($my_posts as $post){
+                setup_postdata($post); ?>
+                <p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p> <?php
+                }
 
-            <?php endwhile; ?>
+                wp_reset_postdata(); ?>
 
         </main>
 
